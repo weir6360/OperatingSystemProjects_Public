3bd3f1466aabf70d7933a73747ad967ffd4bbd85
NOPE <superghostbuster99@gmail.com>
2021-10-17 14:51:41 -0400

    some updates to t.c, mostly comments on process

--- gale_weir_1/t.c
+++ gale_weir_1/t.c
@@ -2,8 +2,17 @@
     Author: Alex Gale, Gabe Weir
     Assignment Number: 1
     Date of Submission:
-    Name of this file: c.c
+    Name of this file: t.c
     Description of the program: Handles the -t command for the 5ps
+*/
+
+/*
+This information is found in the stat file in process's directory, looking at the "utime" field.
+Add to this to the system time, “stime” field . This is the total time consumed by the
+process. This time is represented in clock ticks. You must divide my number of clock
+ticks per second (system constant) to obtain the number of seconds. It should then be
+displayed in hours:min:seconds format. If this option is not present, do not display this
+information.
 */
 
 #include <stdio.h>
@@ -19,7 +28,13 @@ Brief description of the task: calls read_stime() and read_utime() to get the ti
     and calls format_timestr(clock_ticks) to produce the desired formatted time string
 */
 
-const char* t_return() { 
+const char* t_return(/*PID?*/) { 
+//call read_ctps
+//call format_timestr
+//return formatted timestring
+ctps = read_ctps();
+timeString = format_timestr(ctps);
+return timeString;
 
 }
 
@@ -32,7 +47,14 @@ Brief description of the task: the function makes a system call to the stat file
 */
 
 int read_utime() { 
+//open stat file
+//read utime field
+//return utime field
 
+
+//fopen(/proc/<pid>/stat)
+//14 utime
+//return utime
 }
 
 /*
@@ -44,6 +66,14 @@ Brief description of the task: the function makes a system call to the stat file
 */
 
 int read_stime() { 
+//open stat file
+//read utime field
+//return utime field
+
+
+//fopen(/proc/<pid>/stat)
+//15 stime
+//return stime
 
 }
 
@@ -56,7 +86,9 @@ Brief description of the task: the function makes a system call for the number o
 */
 
 int read_ctps() { 
+//find and return clock ticks value for system, figure out what file/call for this
 
+//it's just 1 million? 
 }
 
 /*
@@ -71,7 +103,13 @@ Brief description of the task:
  */
 
 const char* format_timestr(clock_ticks){ 
-    
+//clock ticks as input
+//call read_stime
+//call read_utime (not necessarially in that order)
+//add them together accordingly
+//format into nice looking string
+
+
 }
 
 
