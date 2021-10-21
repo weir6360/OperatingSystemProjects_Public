3a9aa592d6efa53d3c166290e802864b2eb13635
Gabriel Weir <weir6360@kettering.edu>
2021-10-21 15:31:04 -0400

    Big changes
    Added code to c.c, v.c and s.c (all need testing), removed unused statmParser and statParser files

--- gale_weir_1/s.c
+++ gale_weir_1/s.c
@@ -20,23 +20,32 @@
 #include <unistd.h>
 #include <zconf.h>
 
+/*
+Function Name: s_return
+Input to the method: pid: the process id for the current process
+Output(Return value): 
+Brief description of the task: 
+*/
+
 const char* s_return(int pid) {
-    char progam_state
+    char[256] program_state;
     
     //copies the contents at the file path /proc/<pid_path>/stat
-    char *stat_file_path
-    strcpy(stat_file_path, ("/proc/%d/stat", pid_path)); 
+    char *stat_file_path;
+    strcpy(stat_file_path, ("/proc/%d/stat", pid)); 
     FILE *stat_file_pointer = fopen(stat_file_path, "r");
     
+    //Return a failed state if file pointer is invalid
     if (stat_file_pointer == NULL) {
         //failed state
         return 1;
     }
 
+    //Return a failed state if the file cannot be scanned
     if (fscanf(state_file_pointer, "%*d %*s %c", program_state) < 1) {
         return 1;
     }
 
     fclose(stat_file_pointer);
-    return program_state
+    return program_state;
 }
