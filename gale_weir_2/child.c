428a3eac4673896fe646c1753b78c229a2ca9f2c
Gabriel Weir <weir6360@kettering.edu>
2021-11-03 14:28:40 -0400

    Creation of files and Prime
    Started work on random generation of a prime number for a child object.

---
+++ gale_weir_2/child.c
@@ -0,0 +1,22 @@
+/*
+    Author: Alex Gale, Gabe Weir
+    Assignment Number: 1
+    Date of Submission: 
+    Name of this file: child.c
+    Description of the program: Child instances  
+*/
+
+int child_prime
+int child_process_num
+int child_pid
+
+
+int main (int argcm char **argv) {
+    print ("Process %d: my PID is %d: I just got started. \nI am starting with the number %d to find the next prime number.",
+    child_process_num, child_pid, child_base_prime);
+}
+
+long unsigned int generate_random_number() { 
+    unsigned long long int lower_bound = 1000000000;
+    unsigned long long int upper_bound = 9999999999;
+}
--- gale_weir_2/timer.c
+++ gale_weir_2/timer.c
@@ -2,7 +2,6 @@
     Author: Alex Gale, Gabe Weir
     Assignment Number: 1
     Date of Submission: 
-    Name of this file: srtfScheduler.c
-    Description of the program: 
+    Name of this file: child.c
+    Description of the program: Child instances  
 */
-
