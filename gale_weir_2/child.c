102dce9517721ea669916cfe4ddf2640180e354e
Gabriel Weir <weir6360@kettering.edu>
2021-11-03 16:07:08 -0400

    Child progress
    - Finished function for prime number generation (tested via online compiler)
    - Finished prime number check (needs testing)

--- gale_weir_2/child.c
+++ gale_weir_2/child.c
@@ -6,17 +6,70 @@
     Description of the program: Child instances  
 */
 
-int child_prime
-int child_process_num
-int child_pid
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int child_pid = -1;
+int child_process_num = -1;
+unsigned long long int child_prime = 1000000000; 
+unsigned long long int highest_prime = 1000000000; 
 
-int main (int argcm char **argv) {
-    print ("Process %d: my PID is %d: I just got started. \nI am starting with the number %d to find the next prime number.",
-    child_process_num, child_pid, child_base_prime);
-}
-
-long unsigned int generate_random_number() { 
+unsigned long long int generate_random_number() { 
     unsigned long long int lower_bound = 1000000000;
     unsigned long long int upper_bound = 9999999999;
+    do { 
+        child_prime *= RAND_MAX;
+        child_prime += rand(); 
+    } while (child_prime < upper_bound && child_prime > 0);
+    if (child_prime < 0) { 
+        child_prime *= -1; 
+    }
+//    printf("%llu", child_prime);
+    return child_prime % 9999999999;
+
+}
+
+int check_prime_num (unsigned long long int current_prime) { 
+    unsigned long long int range = sqrt(check); 
+    unsigned long long int iterator = 3; 
+    int is_prime = 1; 
+    while (is_prime == 1 && iterator <= range) {
+        if ((current_prime % iterator) == 0) {
+            is_prime = 0; 
+        }
+        iterator++; 
+    }
+    return is_prime; 
+}
+
+void check_child_args(int argc, char *argv[]) { 
+
+}
+
+void check_child_signal(int signal) {
+    switch (signal) { 
+        case SIGCONT: 
+            printf("Process %d: my PID is %d: I ", child_process_num, child_pid, highest_prime); 
+            pause(); 
+            break; 
+        case SIGTERM: 
+            printf("Process %d: my PID is %d: I ", child_process_num, child_pid, highest_prime); 
+            exit(EXIT_SUCCESS);
+            break; 
+        case SIGTSTP: 
+            printf("Process %d: my PID is %d: I ", child_process_num, child_pid, highest_prime); 
+            pause(); 
+            break;
+    }
+}
+
+int main (int argc, char **argv) {
+    time_t seed_time;
+    srand((unsigned) time(&seed_time));
+    unsigned long long int holder = generate_random_number();
+    child_prime = holder;
+    printf("Process %d: my PID is %d: I just got started. \nI am starting with the number %llu to find the next prime number.\n",
+    child_process_num, child_pid, child_prime);
 }
