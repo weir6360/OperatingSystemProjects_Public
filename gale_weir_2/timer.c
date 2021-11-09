933c8d19ca06d73a1721059607c9bcfc806600e5
Gabriel Weir <weir6360@kettering.edu>
2021-11-09 12:43:43 -0500

    Timer function done, need handler

--- gale_weir_2/timer.c
+++ gale_weir_2/timer.c
@@ -5,3 +5,31 @@
     Name of this file: child.c
     Description of the program: Child instances  
 */
+
+void timer_handler(int timer_signal)
+
+void start_timer() { 
+    struct sigaction sa; 
+    memset (&sa, 0, sizeof(sa)); 
+    sa.sa_handler = &timer_handler; 
+    sigaction(SIGALRM, &sa, NULL);    
+
+    struct itimerval timer; 
+    timer.it_value.tv_sec = 1; 
+    timer.it_vaule.tv_usec = 0; 
+    timer.it_interval.tv_sec = 1; 
+    timer.it_interval.tv_usec = 0; 
+    
+    if (setitimer(ITIMER_REAL, &timer, NULL) == -1) {
+        printf("%s: error caused by setitimer.\n", prog_name);
+        exit(EXIT_FAILURE)
+    }
+
+    while (1) {
+        
+    }
+
+
+
+}
+
