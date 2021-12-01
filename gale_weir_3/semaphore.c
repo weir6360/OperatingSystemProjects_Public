8929eb55234fded2605bdf4a6b564edb41f6bb9a
Gabriel Weir <weir6360@kettering.edu>
2021-12-01 15:32:34 -0500

    Auto stash before merge of "assignment_3" and "origin/assignment_3"

---
+++ gale_weir_3/semaphore.c
@@ -0,0 +1,14 @@
+/*
+    Author: Alex Gale, Gabe Weir
+    Assignment Number: x
+    Date of Submission: xx/xx/xx
+    Name of this file: ___
+    Description of the program: ___________
+        ___________________________________
+*/
+
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
