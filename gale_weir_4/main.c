c724374488a2403c568d3888f69e11d425f2ca9d
NOPE <superghostbuster99@gmail.com>
2021-12-10 18:03:57 -0500

    didn't really do anything, file reading still refuses to work

--- gale_weir_4/main.c
+++ gale_weir_4/main.c
@@ -7,3 +7,33 @@
         4. Physical Address (Physical page number * page size + offset)
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char* address_file = "addresses.txt";
+    char* process_file = "PROCESS.txt";
+
+    FILE* address_reader = fopen(address_file, "r");
+    if(address_reader == NULL) {
+        printf("address file not found");
+        exit(0);
+    }
+    
+    FILE* process_reader = fopen(process_file, "r");
+    if(process_reader == NULL) {
+        printf("PROCESS file not found");
+        exit(0);
+    }
+
+    //this will be a function and/or a loop, will process one line. will run 300 times
+    int i;
+    int logical_address;
+    
+    for(i = 0; i < 300; i++){
+        fscanf(address_reader, "%d", &logical_address);
+        
+        printf("logical address: %d\n", logical_address);
+    }
+}
